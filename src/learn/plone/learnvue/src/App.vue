461b1d56dac542fabba0363e0131d6a078be4d47
poliyka <jkny3586@gmail.com>
2021-07-31 19:35:02 +0800

    remove some thing

--- src/learn/plone/learnvue/src/App.vue
+++ src/learn/plone/learnvue/src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div style="display: inline-flex">
-      <Card style="width: 20rem; margin: 5px">
-        <template #header>
-          <img
-            alt="user header"
-            src="#"
-          >
-        </template>
-        <template #title>
-          Advanced Card
-        </template>
-        <template #content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-          quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-        </template>
-        <template #footer>
-          <Button
-            icon="pi pi-check"
-            label="Save"
-          />
-          <Button
-            icon="pi pi-times"
-            label="Cancel"
-            class="p-button-secondary"
-            style="margin-left: .5em"
-          />
-        </template>
-      </Card>
+    <Card style="width: 20rem; margin: 5px">
+      <template #header>
+        <img
+          alt="user header"
+          src="#"
+        >
+      </template>
+      <template #title>
+        Advanced Card
+      </template>
+      <template #content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+        quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+      </template>
+      <template #footer>
+        <Button
+          icon="pi pi-check"
+          label="Save"
+        />
+        <Button
+          icon="pi pi-times"
+          label="Cancel"
+          class="p-button-secondary"
+          style="margin-left: .5em"
+        />
+      </template>
+    </Card>
   </div>
 
 </template>
@@ -53,5 +53,4 @@ export default defineComponent({
     Card,
   },
 });
-
 </script>
